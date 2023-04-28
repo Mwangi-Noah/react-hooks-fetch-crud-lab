7f6e15e0551f5fd628fa6e950f54fbdf471c8bd0
Noah <noahmwangi@gmail.com>
2023-04-28 07:21:29 +0300

    Completed Deliverable 1

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  //a function to handle additional questions
+  /*function handleAddQuestion(newQuestion){
+    setQuestions([...questions, newQuestion]);
+    <QuestionForm onAddQuestion={handleAddQuestion} />
+
+  }*/
+    
   return (
-    <section>
+    <section className="QuestionList">
+   
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map((question) => (
+        <QuestionItem
+          key = {question.id}
+          question = {question}
+          />
+      ))}
+      </ul>
     </section>
   );
 }
